e928a5cc4f44b7227b75ee5a0520b64d5144d681
Aamod Varma <aamodvarma@gmail.com>
2021-08-18 23:01:21 +0400

    s

--- bookswitch/settings.py
+++ bookswitch/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
