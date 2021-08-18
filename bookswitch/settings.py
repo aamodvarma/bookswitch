570c7e26ac727cf12868a85514067ce0381b250f
Aamod Varma <aamodvarma@gmail.com>
2021-08-19 01:32:13 +0400

    sd

--- bookswitch/settings.py
+++ bookswitch/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_env'),
 ]
 
-MEDIA_URL = '/static/images/'
+MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
