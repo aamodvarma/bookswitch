6193485790b8b78e14d72ac17864ac8318c00c61
Aamod Varma <aamodvarma@gmail.com>
2021-08-19 00:46:26 +0400

    sd

--- bookswitch/settings.py
+++ bookswitch/settings.py
@@ -137,9 +137,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_env'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/images/'
